c0638d39302e7e20a0bcc1d94120e349f2be0249
Raoul Lefmann <raoul.lefmann@gmail.com>
2020-12-13 12:13:29 +0100

    Day 10, pt. 2

--- 10.c
+++ 10.c
@@ -52,6 +52,38 @@ list_add(list_t * lst, int item)
 }
 
 
+void
+list_read_from_file(list_t * list, char * filepath)
+{
+	FILE * fp = fopen(filepath, "r");
+	if (fp == NULL) {
+		fprintf(stderr, "Error opening file `%s`.\n", filepath);
+		exit(EXIT_FAILURE);
+	}
+
+	char * line = NULL;
+	size_t len;
+	ssize_t nread;
+
+	int value;
+	for (;;) {
+		nread = getline(&line, &len, fp);
+		if (nread == -1) {
+			break;
+		}
+		line[nread - 1] = '\0';
+		nread = sscanf(line, "%d\n", &value);
+		if (nread != 1) {
+			fprintf(stderr, "Error processing line `%s`.\n", line);
+			exit(EXIT_FAILURE);
+		}
+		list_add(list, value);
+	}
+	free(line);
+	fclose(fp);
+}
+
+
 int
 compareint(const void * a, const void * b)
 {
@@ -75,19 +107,35 @@ list_sort(list_t * list)
 
 
 void
-count_joltage_differences(list_t * joltages, int * num_one_jolt_differences, int * num_three_jolt_differences)
+create_joltage_list(list_t * list, char * filepath)
 {
-	*num_one_jolt_differences = 0;
-	*num_three_jolt_differences = 0;
-	int prev_joltage = 0;
+	// add charging outlet:
+	list_add(list, 0);
+
+	list_read_from_file(list, filepath);
+
+	list_sort(list);
+
+	// add device's built-in adapter:
+	int max_joltage = list->items[list->len - 1];
+	list_add(list, max_joltage + 3);
+}
+
+
+void
+count_joltage_differences(list_t * joltages, int * num_diff_1_jolt, int * num_diff_3_jolt)
+{
+	*num_diff_1_jolt = 0;
+	*num_diff_3_jolt = 0;
+	int prev_joltage = joltages->items[0];
 	int diff;
-	for (int i = 0; i < joltages->len; i++) {
+	for (int i = 1; i < joltages->len; i++) {
 		diff = joltages->items[i] - prev_joltage;
 
 		if (diff == 1) {
-			(*num_one_jolt_differences)++;
+			(*num_diff_1_jolt)++;
 		} else if (diff == 3) {
-			(*num_three_jolt_differences)++;
+			(*num_diff_3_jolt)++;
 		} else {
 			fprintf(stderr, "Bad joltage difference: %d.\n", diff);
 			exit(EXIT_FAILURE);
@@ -95,9 +143,50 @@ count_joltage_differences(list_t * joltages, int * num_one_jolt_differences, int
 
 		prev_joltage = joltages->items[i];
 	}
-	// the device's built-in adapter is always 3 higher
-	// than the highest adapter
-	(*num_three_jolt_differences)++;
+}
+
+
+long
+count_paths(list_t * joltages)
+{
+	int num_adapters = joltages->len;
+	long * num_ways_to_reach_adapter = calloc(num_adapters, sizeof(long));
+	num_ways_to_reach_adapter[0] = 1;
+
+	for (int i = 1; i < num_adapters; i++) {
+		// find previous adapters:
+		for (int j = i - 1; j >= i - 3; j--) {
+			if (j < 0) {
+				break;
+			} else if (joltages->items[j] < joltages->items[i] - 3) {
+				break;
+			}
+			num_ways_to_reach_adapter[i] += num_ways_to_reach_adapter[j];
+		}
+	}
+	long num_paths = num_ways_to_reach_adapter[num_adapters - 1];
+	free(num_ways_to_reach_adapter);
+	return num_paths;
+}
+
+
+void
+part1(list_t * list)
+{
+	int num_diff_1_jolt;
+	int num_diff_3_jolt;
+	count_joltage_differences(list, &num_diff_1_jolt, &num_diff_3_jolt);
+	printf("1:\t%d\n", num_diff_1_jolt);
+	printf("3:\t%d\n", num_diff_3_jolt);
+	printf("prod:\t%d\n", num_diff_1_jolt * num_diff_3_jolt);
+}
+
+
+void
+part2(list_t * list)
+{
+	long num_paths = count_paths(list);
+	printf("paths:\t%ld\n", num_paths);
 }
 
 
@@ -112,44 +201,11 @@ main(int argc, char * argv[])
 
 	list_t list;
 	list_init(&list);
+	create_joltage_list(&list, filepath);
 
-	FILE * fp = fopen(filepath, "r");
-	if (fp == NULL) {
-		fprintf(stderr, "Error opening file `%s`.\n", filepath);
-		return EXIT_FAILURE;
-	}
+	part1(&list);
+	part2(&list);
 
-	char * line = NULL;
-	size_t len;
-	ssize_t nread;
-
-	int value;
-	for (;;) {
-		nread = getline(&line, &len, fp);
-		if (nread == -1) {
-			break;
-		}
-		line[nread - 1] = '\0';
-		nread = sscanf(line, "%d\n", &value);
-		if (nread != 1) {
-			fprintf(stderr, "Error processing line `%s`.\n", line);
-			return EXIT_FAILURE;
-		}
-		list_add(&list, value);
-	}
-
-	list_sort(&list);
-
-	int num_one_jolt_differences;
-	int num_three_jolt_differences;
-	count_joltage_differences(&list, &num_one_jolt_differences, &num_three_jolt_differences);
-
-	printf("1: %d\n", num_one_jolt_differences);
-	printf("3: %d\n", num_three_jolt_differences);
-
-	printf("prod: %d\n", num_one_jolt_differences * num_three_jolt_differences);
-
-	free(line);
 	list_free(&list);
 
 	return EXIT_SUCCESS;
